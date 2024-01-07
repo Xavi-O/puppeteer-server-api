d065660385974e812a4d90c68e0536fe09345e78
Xavier <odhiamboxavier@gmail.com>
2024-01-07 17:43:22 +0300

    updates on new logic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
+    defaultViewport: null,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -15,47 +16,64 @@ const scrapeLogic = async (res) => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
-  const page = await browser.newPage();
+  try {
+    let page = await browser.newPage();
+    await page.setRequestInterception(true);
 
-  await page.goto("https://glovoapp.com/ke/en/nairobi/kfc-nbo/", {
-    waitUntil: "networkidle0",
-  });
+    page.on("request", (req) => {
+      if (
+        req.resourceType() == "stylesheet" ||
+        req.resourceType() == "font" ||
+        req.resourceType() == "image"
+      ) {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
+    await page.goto("https://glovoapp.com/ke/en/nairobi/kfc-nbo/", {
+      waitUntil: "networkidle0",
+    });
 
-  // Set screen size
-  await page.setViewport(null);
+    const [search] = await page.$x('//*[@class="search-input"]');
+    if (search) {
+      await search.click();
+    }
 
-  const [search] = await page.$x('//*[@class="search-input"]');
-  if (search) {
-    await search.click();
-  }
-
-  let searchText = await page.waitForXPath('//*[@class="search-input__field"]');
-  await searchText.type("doublecrunchburger");
-
-  await page.waitForSelector(".product-row", {
-    visible: true,
-  });
-
-  const productHandles = await page.$$(".store__body__dynamic-content");
-  for (const productHandle of productHandles) {
-    title = await page.evaluate(
-      (el) =>
-        el.querySelector(".product-row__content > div > div.product-row__name")
-          .textContent,
-      productHandle
+    let searchText = await page.waitForXPath(
+      '//*[@class="search-input__field"]'
     );
+    await searchText.type("double crunch burger");
 
-    price = await page.evaluate(
-      (el) =>
-        el.querySelector(
-          ".product-price__effective.product-price__effective--new-card"
-        ).textContent,
-      productHandle
-    );
-    console.log(title, price);
+    await page.waitForSelector(".product-row", {
+      visible: true,
+    });
+
+    const productHandles = await page.$$(".store__body__dynamic-content");
+    for (const productHandle of productHandles) {
+      title = await page.evaluate(
+        (el) =>
+          el.querySelector(
+            ".product-row__content > div > div.product-row__name"
+          ).textContent,
+        productHandle
+      );
+
+      price = await page.evaluate(
+        (el) =>
+          el.querySelector(
+            ".product-price__effective.product-price__effective--new-card"
+          ).textContent,
+        productHandle
+      );
+      console.log(title, price);
+    }
+  } catch (e) {
+    console.error(e);
+    console.log(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
   }
-
-  await browser.close();
 };
 
 module.exports = { scrapeLogic };
