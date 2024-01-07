89733256eef3b5f73b37ee07797cf030f402cd19
Xavier <odhiamboxavier@gmail.com>
2024-01-07 19:26:43 +0300

    functions moved to single file

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const scrapeLogic = async (res) => {
+  const browser = await puppeteer.launch({
+    defaultViewport: null,
+    headless: false,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+      "--force-device-scale-factor=0.5",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    const page = await browser.newPage();
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
+    await page.goto(
+      "https://glovoapp.com/ke/en/nairobi/kfc-nbo?search=double%20crunch%20burger",
+      { waitUntil: "domcontentloaded" }
+    );
+
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      ".product-row__content > div > div.product-row__name"
+    );
+    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+
+    // Print the full title
+    const logStatement = `The title of this product is ${fullTitle}`;
+    console.log(logStatement);
+    app.get("/scrape", (req, res) => {
+      res.send(logStatement);
+    });
+  } catch (e) {
+    console.error(e);
+    app.get("/scrape", (req, res) => {
+      res.send(`Something went wrong while running Puppeteer: ${e}`);
+    });
+  } finally {
+    await browser.close();
+  }
+};
+scrapeLogic();
+
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
