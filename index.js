217819376cd9d63e9879ac8a56cc3fd9083cab79
Xavier <odhiamboxavier@gmail.com>
2024-01-07 22:04:15 +0300

    waiting for network idle

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const scrapeLogic = async (res) => {
 
     await page.goto(
       "https://glovoapp.com/ke/en/nairobi/kfc-nbo?search=double%20crunch%20burger",
-      {waitUntil: 'domcontentloaded', timeout: 0}
+      {waitUntil: 'networkidle0', timeout: 0}
     );
 
     // Locate the full title with a unique string
