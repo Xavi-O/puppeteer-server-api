c90fd3cae5c0b101a17215f50dd98229b8f30925
Xavier <odhiamboxavier@gmail.com>
2024-01-07 19:57:09 +0300

    root inside async

--- index.js
+++ index.js
@@ -46,14 +46,13 @@ const scrapeLogic = async (res) => {
   } finally {
     await browser.close();
   }
+  app.get("/", (req, res) => {
+    res.send("Render Puppeteer server is up and running!");
+  });
 };
 scrapeLogic();
 
 const PORT = process.env.PORT || 4000;
-
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
